252c52c6b16616fe2827ae9adf0cec75d0d4e476
Jayeshkikani03 <jkkikani2003@gmail.com>
2026-02-14 16:50:33 +0530

    feat: Integrate Capacitor for mobile app development and update dependencies.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,19 +8,7 @@ export default defineConfig({
     open: true
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true
-  },
-  optimizeDeps: {
-    disabled: false,
-    include: ['react', 'react-dom', 'react-router-dom', 'lucide-react', 'recharts'],
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  },
-  define: {
-    global: 'globalThis'
   }
-});
+})
